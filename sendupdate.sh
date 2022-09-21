3d7d7116677440bf8bb6a68dbb16a2c5984d7be8
Tom Hansen <tomh@uwm.edu>
2022-09-21 14:51:52 -0500

    front display changes

--- sendupdate.sh
+++ sendupdate.sh
@@ -2,7 +2,7 @@
 /usr/bin/sleep 30
 SRC=/calendars/GLRF_ALL.ics
 OLD=/tmp/GLRF_OLD.ics
-[ -f $OLD ] && diff -q $SRC $OLD && exit 0
+[ -f $OLD ] && diff $SRC $OLD > /tmp/diff$$.txt && exit 0
 
-curl --url 'smtp://smtprelay.uwm.edu:25' --mail-from 'tomh@uwm.edu' --mail-rcpt 'tomh@uwm.edu' -F '=Upload attachment to https://sites.uwm.edu/sfs-reservations/wp-admin/edit.php?post_type=calendar_event&page=calendar-plus-settings&tab=import ;type=text/plain' -F attachment=@$SRC -H 'Subject: ROAR calendar update' && cp $SRC $OLD
+curl --url 'smtp://smtprelay.uwm.edu:25' --mail-from 'tomh@uwm.edu' --mail-rcpt 'tomh@uwm.edu' -F "=@/tmp/diff$$.txt;type=text/plain" -F "attachment=@$SRC" -H 'Subject: ROAR calendar update' && cp $SRC $OLD
 
