1cc356e849d9f2bcaa0327aca59576c12bf94ed4
Tom Hansen <tomh@uwm.edu>
2022-09-28 10:57:42 -0500

    fixed /bin/sleep

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,19 @@ FROM php:7.4-cli
 MAINTAINER Tom Hansen "tomh@uwm.edu"
 
 # update packages needed
+RUN apt-get update
 RUN apt-get install --fix-missing
 RUN apt-get update
-RUN apt-get upgrade -y
-RUN apt-get install --fix-missing
-RUN apt-get -y install wget cron
-RUN apt-get install --fix-missing
+# RUN apt-get upgrade -y
+RUN apt-get -y install --fix-missing
+RUN apt-get -y install apt-utils
+RUN apt-get -y install --fix-missing
+RUN apt-get -y install wget 
+RUN apt-get -y install --fix-missing
+RUN apt-get -y install cron
+RUN apt-get -y install --fix-missing
+RUN apt-get -y install coreutils 
+RUN apt-get -y install --fix-missing
 
 # copy local content to its container home
 COPY . /opt/roardaemon
--- sendupdate.sh
+++ sendupdate.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-/usr/bin/sleep 30
 
+/bin/sleep 10
 
-for f in /calendars/GLRF_*.ics; do
+for f in /calendars/GLRF_ALL.ics; do
 	SRC=$f
-	OLD=/tmp/`basename $f .ics`.old
-	[ -f $OLD ] && diff $SRC $OLD && exit 0
+	# OLD=/tmp/`basename $f .ics`.old
+	# [ -f $OLD ] && diff $SRC $OLD && exit 0
 
-	curl --url 'smtp://smtprelay.uwm.edu:25' --mail-from 'tomh@uwm.edu' --mail-rcpt 'tomh@uwm.edu' -F '=Upload attachment to https://sites.uwm.edu/sfs-reservations/wp-admin/edit.php?post_type=calendar_event&page=calendar-plus-settings&tab=import ;type=text/plain' -F attachment=@$SRC -H 'Subject: ROAR calendar update' && cp $SRC $OLD
-
+	/usr/bin/curl --url 'smtp://smtprelay.uwm.edu:25' --mail-from 'tomh@uwm.edu' --mail-rcpt 'tomh@uwm.edu' -F '=Upload attachment to https://sites.uwm.edu/sfs-reservations/wp-admin/edit.php?post_type=calendar_event&page=calendar-plus-settings&tab=import ;type=text/plain' -F attachment=@$SRC -H 'Subject: ROAR calendar update'  #  && cp $SRC $OLD
+	/bin/sleep 0.1
 done
